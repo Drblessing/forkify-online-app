6a87e6762bc0f38586146b6c62fc80ecf3f218e8
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-05-23 21:02:48 -0700

    sorted imports

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,4 @@
-import { API_URL, RES_PER_PAGE, KEY } from './config.js';
+import { API_URL, KEY, RES_PER_PAGE } from './config.js';
 // import { getJSON, sendJSON } from './helpers.js';
 import { AJAX } from './helpers.js';
 export const state = {
