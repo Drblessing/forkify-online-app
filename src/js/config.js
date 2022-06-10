d1b2a85c34f6ae19ca4e6548dc99a3d7b2a2b20c
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-06-09 21:56:56 -0700

    adding .env

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '9cef0944-3b80-4609-bfd8-c3e4345a2ea5';
+export const KEY = process.env.KEY;
 export const MODAL_CLOSE_SECONDS = 2.5;
